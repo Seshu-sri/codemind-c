ce28bb09c2082c0d9e5afb4be0ec8e3b29d6ccc9
Seshu-sri <100022250+Seshu-sri@users.noreply.github.com>
2022-02-28 08:16:49 +0530

    #include<stdio.h> int main() {     int n,rem,largest=0;     scanf("%d",&n);     while(n>0)     {         rem=n%10;         if(largest<rem)         {             largest=rem;         }         n=n/10;     }     printf("%d",largest);     return 0; }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,rem,largest=0;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        rem=n%10;
+        if(largest<rem)
+        {
+            largest=rem;
+        }
+        n=n/10;
+    }
+    printf("%d",largest);
+    return 0;
+} 
